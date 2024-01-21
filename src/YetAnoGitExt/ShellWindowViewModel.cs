aa3251f8ed734ed7e6fe5e7afc26577cfbd29c84
Ghislain Z <gzeleu@outlook.de>
2024-01-21 02:16:47 +0100

    Continous integration  hasve been improved

---
+++ src/YetAnoGitExt/ShellWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿// <copyright company="Ghislain One Inc.">
+//  Copyright (c) GhislainOne
+//  This computer program includes confidential, proprietary
+//  information and is a trade secret of GhislainOne. All use,
+//  disclosure, or reproduction is prohibited unless authorized in
+//  writing by an officer of Ghis. All Rights Reserved.
+// </copyright>
+
+namespace YetAnoGitExt;
+
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public  class ShellWindowViewModel: BindableBase
+{
+    private string filePath = "C:\\git\\ZoeProg";
+    public string FilePath
+    {
+        get =>  this.filePath; 
+        set => SetProperty(ref this.filePath, value); 
+    }
+}
